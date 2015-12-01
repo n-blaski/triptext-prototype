443cabdd16f9a4079b0e52f685f7dc22261072dd
n-blaski <webstudent@PC16287.local>
2015-11-30 18:36:23 -0600

    Added Parse
    Added parse and maps support. Got rid of Conversations for the time
    being.
    
    Goal: Show the location of the user’s contacts on the map.

--- TripText/AppDelegate.swift
+++ TripText/AppDelegate.swift
@@ -9,6 +9,9 @@
 import UIKit
 import CoreData
 
+import Parse
+import Bolts
+
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
@@ -16,6 +19,16 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
+        // [Optional] Power your app with Local Datastore. For more info, go to
+        // https://parse.com/docs/ios_guide#localdatastore/iOS
+        Parse.enableLocalDatastore()
+        
+        // Initialize Parse.
+        Parse.setApplicationId("XImyFDzahYdUzVY2lxz0Wlr8j4sJQzUCuXAdFdtN",
+            clientKey: "gc2vEBhDpLOhGeaeLBoo7fRmINOk12EyWUJGnsGb")
+        
+        // [Optional] Track statistics around application opens.
+        PFAnalytics.trackAppOpenedWithLaunchOptions(launchOptions)
         // Override point for customization after application launch.
         return true
     }
